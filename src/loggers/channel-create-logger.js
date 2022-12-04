17aa5f24d03a0250a64b4bee458ae04fa07822b0
kafka050 <kafka050@umn.edu>
2022-12-04 16:49:41 -0600

    Update channel-create-logger.js

--- src/loggers/channel-create-logger.js
+++ src/loggers/channel-create-logger.js
@@ -10,13 +10,13 @@ module.exports = (channel) => {
   if (channel.type === ChannelType.DM || channel.type === ChannelType.GroupDM || channel.guild.id !== main_server) {
     return null
   }
-  const embed = {
+  /*const embed = {
     color: colors.blue,
     title: 'Channel created',
     thumbnail: {
       url: images.ibex.blue,
     },
     fields: [{ name: 'Channel', value: channel.toString() }],
-  }
-  return embed
+  }*/
+  return 'nah'
 }
